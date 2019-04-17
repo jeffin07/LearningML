519b7cb2bda377038c2c8b7095cc0fa622c47156
jeffin07 <francisjeffin07@gmail.com>
2019-04-17 11:29:44 +0530

    clean ups

--- implementation/vgg/data_pipeline.py
+++ implementation/vgg/data_pipeline.py
@@ -1,8 +1,12 @@
 from PIL import Image
 import os
 import numpy as np 
+import matplotlib.pyplot as plt
 import cv2
 import random
+import tensorflow as tf
+# import vgg_tf
+
 
 class Dataset:
 	def __init__(self,dataset_path):
@@ -31,33 +35,24 @@ class Dataset:
 					raise e
 					continue
 			self.data_path.append(good_images)
-		# label_names.close()
-		# train_names = open(os.path.join(dataset_path, "train.txt"), "w")
-		# test_names = open(os.path.join(dataset_path, "test.txt"), "w")
-		# random.shuffle(good_images)
-		# split = 0
-		# print("splitting train/test")
-		# for i in good_images:
-		# 	split+=1
-		# 	if split == 5:
-		# 		test_names.write(i+"\n")
-		# 		split=0
-		# 	else:
-		# 		train_names.write(i+"\n")
-		# train_names.close()
-		# test_names.close()
-		# train_names = open(os.path.join(dataset_path, "train.txt"), "r").readlines()
-		# self.train_names = [i.split("\n")[0] for i in train_names]
-		# test_names = open(os.path.join(dataset_path, "test.txt"), "r").readlines()
-		# self.test_names = [i.split("\n")[0] for i in test_names]
+	
+	def resize_image(self,img, size):
+		h,w  = img.shape[:2]
+		sh, sw = size
+		if h > sh or w > sw:  # shrinking image
+			interp = cv2.INTER_AREA
+		else: # stretching image
+			interp = cv2.INTER_CUBIC
+		new_img = cv2.resize(img, size, interpolation=interp)
+		return new_img
 
 	def get_batch(self, batch_size=4):
-		print(self.cats)
+		# print(self.cats)
 		images = []
 		labels = []
 		empty = False
 		counter = 0
-		each_cat_no = batch_size/len(self.dataset_path)
+		each_cat_no = batch_size/len(self.cats)
 		while True:
 			for i in range(len(self.cats)):
 				label = np.zeros(len(self.cats),dtype=int)
@@ -65,14 +60,16 @@ class Dataset:
 				if len(self.data_path[i]) < counter+1:
 					empty = True
 					continue
+				empty=False
 				img = cv2.imread(self.data_path[i][counter])
+				img = self.resize_image(img,(224,224))
 				images.append(img)
 				labels.append(label)
-			counter+1
+			counter+=1
 
 			if empty:
 				break
-			if counter%batch_size == 0:
+			if (counter) % each_cat_no == 0:
 				yield np.array(images,dtype=np.uint8),np.array(labels,dtype=np.uint8)
 				del images
 				del labels
@@ -84,8 +81,3 @@ class Dataset:
 
 
 
-sample_data = Dataset('/home/jeffin/git/Me/LearningML/data/train')
-sample_data.read_dataset()
-for i in range(5):
-	k,j = sample_data.get_batch()
-	print(k)
