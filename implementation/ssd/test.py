5f45c46df349aafbd570b5e345a6e166318ccf52
jeffin07 <francisjeffin07@gmail.com>
2019-05-31 12:14:07 +0530

    Readme added and kindoff a test

--- implementation/ssd/test.py
+++ implementation/ssd/test.py
@@ -1,8 +1,10 @@
 from vgg_16 import Vgg
+from multishot import multishot
 import tensorflow as tf
 
 vvg_out = Vgg()
+num_classes = 1
 input_layer = tf.keras.layers.Input(shape=(300,300,3))
 modified_vgg = vvg_out(input_layer)
-for i in modified_vgg:
-	print(i)
+loc = multishot(modified_vgg,num_classes+1)
+print(loc)
