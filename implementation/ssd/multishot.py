d4a163b7c58bc6181a0c5610daedc203119833e7
jeffin07 <francisjeffin07@gmail.com>
2019-06-01 15:20:18 +0530

    configs added

--- implementation/ssd/multishot.py
+++ implementation/ssd/multishot.py
@@ -1,17 +1,15 @@
 from tensorflow.keras.layers import Conv2D
 
-# not a good way should include config :/
-mbox = [4, 6, 6, 6, 4, 4]
 
-
-def multishot(features_layers, num_classes):
+def multishot(features_layers, num_classes, cfg):
 	'''
 
 	Args
 
 	extra_features : extra feature layers for SSD
-	num_boxes  : number of boxes at each feature layer
-
+	num_classes  : number of classes 
+	cfg : conatains the number of boxes for each layer
+	
 	Output
 
 	list of localization and classification
@@ -23,10 +21,10 @@ def multishot(features_layers, num_classes):
 	for index, layer in enumerate(features_layers):
 		loc_layers += [
 			Conv2D(
-				filters=mbox[index] * 4, kernel_size=3,
+				filters=cfg[index] * 4, kernel_size=3,
 				padding="same")(layer)]
 		conf_layers += [
 			Conv2D(
-				filters=mbox[index] * num_classes, kernel_size=3,
+				filters=cfg[index] * num_classes, kernel_size=3,
 				padding="same")(layer)]
 	return [loc_layers, conf_layers]
--- implementation/ssd/test.py
+++ implementation/ssd/test.py
@@ -1,10 +1,13 @@
 from vgg_16 import Vgg
 from multishot import multishot
 import tensorflow as tf
+from configs import configs
+
 
 vvg_out = Vgg()
 num_classes = 1
 input_layer = tf.keras.layers.Input(shape=(300,300,3))
 modified_vgg = vvg_out(input_layer)
-loc = multishot(modified_vgg,num_classes+1)
+cfg = configs['number_of_boxes']
+loc = multishot(modified_vgg, num_classes+1, cfg)
 print(loc)
