33a2afce45ac9194f79fcd660c06509574fab8ba
jeffin07 <francisjeffin07@gmail.com>
2019-07-02 17:33:53 +0530

    anchor box generation

--- implementation/ssd/test.py
+++ implementation/ssd/test.py
@@ -2,6 +2,7 @@ from vgg_16 import Vgg
 from multishot import multishot
 import tensorflow as tf
 from configs import configs
+from anchor_box import generate_anchors
 
 
 vvg_out = Vgg()
@@ -11,3 +12,5 @@ modified_vgg = vvg_out(input_layer)
 cfg = configs['number_of_boxes']
 loc = multishot(modified_vgg, num_classes+1, cfg)
 print(loc)
+boxes = generate_anchors(configs)
+print(len(boxes))
