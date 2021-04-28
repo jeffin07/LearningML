eb79fe74edac3d1c461a884c32f01703ac674dd5
jeffin07 <francisjeffin07@gmail.com>
2021-04-28 22:10:20 +0530

    senet first

---
+++ implementation/SENet/senet.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+
+
+class SEnet(nn.Module):
+
+	def __init__(self, backbone="vgg"):
+
+		'''
+			this has some more parameters and also initialize layers
+		'''
+		pass
+
+	def sequeeze_and_exite(self):
+		'''
+		
+		'''
+		pass
+
+	def forward(self):
+		'''
+			forward pass
+		'''
+		pass
