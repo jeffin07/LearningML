e5ff71e922ee74e435b76d71fd928a0e3e2e6c2f
jeffin07 <francisjeffin07@gmail.com>
2021-05-21 21:36:05 +0530

    mobilenetv2 architecture done, need to add training

--- implementation/MobilenetV2/mobilenet.py
+++ implementation/MobilenetV2/mobilenet.py
@@ -72,31 +72,40 @@ class MobilenetV2(nn.Module):
 		layers = []
 		input_channel = 32
 		last_channel = 1280
-		avg_pool = nn.AdaptiveAvgPool2d(1)
+		avg_pool = nn.AvgPool2d(7)
+		# avg_pool = nn.AdaptiveAvgPool2d(7)
 		# def forward(self, x):
 		layers.append(convblock(3, input_channel, stride=2))
 		# for i in self.conf:
 
 		for t,c,n,s in self.conf:
 
+			print("^^^^^^",[t,c,n,s])
 			for num in range(n):
-				layers.append(invertedresidualblock(input_channel, c, t, stride=s))
+				stride = s if num ==0 else 1
+				layers.append(invertedresidualblock(input_channel, t, c, stride=stride))
 				input_channel = c
-
+		
 
 		layers.append(convblock(input_channel, last_channel, stride=1))
 		layers.append(avg_pool)
-		layers.append(convblock(input_channel, 1000, stride=1))
+		# layers.append(convblock(last_channel, 1000, stride=1))
 
 		self.features = nn.Sequential(*layers)
+
+		self.classifier = nn.Sequential(
+			nn.Dropout(0.2),
+			nn.Linear(last_channel,1000)
+			)
 
 	def __str__(self):
 
 		return '{}'.format(self.features)
 
 	def forward(self, x):
-
-		return self.features(x)
+		x = self.features(x)
+		x = x.reshape(x.shape[0], -1)
+		return self.classifier(x)
 
 
 
@@ -105,10 +114,16 @@ if __name__ == '__main__':
 
 	input_tensor = torch.rand(1,3,224,224)
 
-	inv = invertedresidualblock(32, 1, 16, stride=1)
+	inv = invertedresidualblock(input_channels=32, expansion_ratio=1, out_channels=16, stride=1)
 	conv = convblock(3, 32, stride=2)
+	print(inv(conv(input_tensor)).size())
+	# exit()
+	# [6,24,2,2]
+	# print((inv(input_tensor)).shape)
+	# inv2=invertedresidualblock(input_channels=24, expansion_ratio=6, out_channels=24, stride=1)
+	# print(inv2(inv(input_tensor)).shape)
 	# Error in forward function
 	# test convb and inverted seperately
 	model = MobilenetV2()
-	print(model(input_tensor))
+	print(model(input_tensor).size())
 	# print(inv(conv(input_tensor)).size())
