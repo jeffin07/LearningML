8c939ec8f1694647724ee1b93d4552fa7ecd4057
jeffin07 <francisjeffin07@gmail.com>
2021-06-12 21:49:19 +0530

    multihead attention simple

--- implementation/ViT/self_attention.py
+++ implementation/ViT/self_attention.py
@@ -2,40 +2,41 @@ import torch
 import torch.nn as nn
 
 
-class self_attention(nn.Module):
+class multi_head_self_attention(nn.Module):
 
     def __init__(self, embed_size, heads=1):
 
-        super(self_attention, self).__init__()
+        super(multi_head_self_attention, self).__init__()
 
         self.embed_size = embed_size
         self.heads = heads
         self.head_dim = self.embed_size // self.heads
 
-        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.keys = nn.Linear(self.embed_size, self.head_dim, bias=False)
+        self.values = nn.Linear(self.embed_size, self.head_dim, bias=False)
+        self.query = nn.Linear(self.embed_size, self.head_dim, bias=False)
 
-        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.softmax = nn.Softmax(dim=1)
 
-        self.query = nn.Linear(self.head_dim, self.head_dim, bias=False)
-
-        # scaled dot product
-        # q*v -> k 
 
     def forward(self, x):
 
         keys = self.keys(x)
         values = self.values(x)
         querys = self.query(x)
-        soft = nn.Softmax(dim=1)
-        out = torch.matmul(soft(torch.matmul(querys,keys)),values)
+
+        attention = torch.matmul(querys, torch.transpose(keys, 0, 1))
+        attention = self.softmax(attention)
+        out = torch.matmul(attention,values)
+
         return out
 
 if __name__ == '__main__':
 
-    x = torch.randn([3,3])
+    x = torch.randn([4,64])
 
-    sa = self_attention(3)
+    mhsa = multi_head_self_attention(64, heads=8)
 
-    print(sa(x))
+    print(mhsa(x))
 
 
