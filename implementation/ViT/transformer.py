9f22370bb922a9359548678d34da09cbeb7d30b7
jeffin07 <francisjeffin07@gmail.com>
2021-06-30 21:15:59 +0530

    transormer

---
+++ implementation/ViT/transformer.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+
+class TransformerBlock(nn.Module):
+
+	def __init__(self)
+		
+		super(TransformerBlock, self).__init__()
+
+		# multi head self attention
+		# add an norm
+		# linear
+		# add and norm
+		pass
+
+
+	def forward(self, keys, values, queries):
+
+		pass
+
